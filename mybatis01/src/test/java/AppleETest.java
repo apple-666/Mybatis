cf2008580a2da7b8bf266276053bb8bbbae952a3
apple <1446727937@qq.com>
2022-03-16 14:31:27 +0800

    again

---
+++ mybatis01/src/test/java/AppleETest.java
@@ -0,0 +1,16 @@
+import org.apache.ibatis.plugin.Interceptor;
+import org.junit.Test;
+
+/**
+ * @Author Double_apple
+ * @Date 2022/3/16 10:30
+ * @Version 1.0
+ */
+public class AppleETest {
+
+    @Test
+    public void t1() {
+
+//        Interceptor;
+    }
+}
